d52b721a48e2dd8ff9fb022a45d208a2e2572baa
TOURNIER THEO <theo.tournier@meteo.fr>
2023-12-07 14:08:36 +0100

    <titan> add grid_info + geopotential_info

--- pnia/titan_dataset.py
+++ pnia/titan_dataset.py
@@ -3,7 +3,7 @@ import numpy as np
 import xarray as xr
 from torch.utils.data import DataLoader, Dataset
 from pnia.base import AbstractDataset
-from cyeccodes.eccodes import get_multi_messages_from_file
+# from cyeccodes.eccodes import get_multi_messages_from_file
 
 class TitanDataset(AbstractDataset, Dataset):
     def __init__(self) -> None:
@@ -21,9 +21,20 @@ class TitanDataset(AbstractDataset, Dataset):
 
     @property
     def grid_info(self) -> np.array:
-        conf_ds = xr.load_dataset(self.ROOT_DIR / "conf.grib", engine='cfgrib')
-        print(conf_ds)
+        conf_ds = xr.load_dataset(self.ROOT_DIR / "conf.grib")
+        latitudes = conf_ds.latitude
+        longitudes = conf_ds.longitude
+        return np.meshgrid(longitudes, latitudes)
 
     @property
     def geopotential_info(self) -> np.array:
+        conf_ds = xr.load_dataset(self.ROOT_DIR / "conf.grib")
+        return conf_ds.h.values
+
+    @property
+    def limited_area(self) -> bool:
+        pass
+
+    @property
+    def border_mask(self) -> bool:
         pass
