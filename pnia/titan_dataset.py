a4503d80c34a43c4e4cdd6ffb9d821c5f7e21a09
TOURNIER THEO <theo.tournier@meteo.fr>
2023-12-07 13:30:21 +0100

    <pnia> update grid info

--- pnia/titan_dataset.py
+++ pnia/titan_dataset.py
@@ -1,14 +1,17 @@
+from pathlib import Path
 import numpy as np
+import xarray as xr
 from torch.utils.data import DataLoader, Dataset
 from pnia.base import AbstractDataset
 
 class TitanDataset(AbstractDataset, Dataset):
     def __init__(self) -> None:
-        pass
+        self.ROOT_DIR = Path("/scratch/shared/Titan/")
 
     @property
     def grid_info(self) -> np.array:
-        pass
+        conf_ds = xr.load_dataset(self.ROOT_DIR / "conf.grib", engine='cfgrib')
+        print(conf_ds)
 
     @property
     def geopotential_info(self) -> np.array:
