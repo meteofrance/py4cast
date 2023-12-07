b037e8edaeabc5ae5a2f2915d84f834f11e1c773
Lea Berthomier <lea.berthomier@meteo.fr>
2023-12-07 13:27:13 +0100

    add methods to titan dataset

--- pnia/base.py
+++ pnia/base.py
@@ -3,11 +3,12 @@ Base classes defining our software components
 and their interfaces
 """
 
+import numpy as np
 from abc import ABCMeta, abstractmethod, abstractproperty
 
 
 class AbstractDataset(ABCMeta):
-    
+
     @abstractproperty
     def grid_info(self) -> np.array:
         pass
--- pnia/titan_dataset.py
+++ pnia/titan_dataset.py
@@ -1,9 +1,20 @@
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
 from pnia.base import AbstractDataset
+from cyeccodes.eccodes import get_multi_messages_from_file
 
 class TitanDataset(AbstractDataset, Dataset):
     def __init__(self) -> None:
+        pass
+
+    def __len__(self):
+        pass
+
+    @staticmethod
+    def load_one_time_step():
+        pass
+
+    def __getitem__(self, index):
         pass
 
     @property
