b03c1135c8831b440476e78dea63d81d61e261f9
Theo Tournier <theo.tournier@meteo.fr>
2023-12-08 15:48:05 +0000

    <neural_lam> working create_grid_features.py

--- pnia/nlam_suede/create_grid_features.py
+++ pnia/nlam_suede/create_grid_features.py
@@ -1,19 +1,19 @@
 import numpy as np
 import torch
-from titan_dataset import AbstractDataset
+from pnia.base import AbstractDataset
+from pnia.titan_dataset import TitanParams, TitanDataset
 from pnia.settings import CACHE_DIR
-
 
 
 def prepare(dataset: AbstractDataset):
     """
     dataset: Dataset to load grid point coordinates from.
     """
-    graph_dir_path = CACHE_DIR / 'neural_lam' / dataset.__str__
+    graph_dir_path = CACHE_DIR / 'neural_lam' / str(dataset)
 
     # -- Static grid node features --
     grid_xy = dataset.grid_info  # (2, N_x, N_y)
-    grid_xy = grid_xy.flatten(1,2).T  # (N_grid, 2)
+    grid_xy = grid_xy.flatten(1, 2).T  # (N_grid, 2)
     pos_max = torch.max(torch.abs(grid_xy))
     grid_xy = grid_xy / pos_max  # Divide by maximum coordinate
 
@@ -31,3 +31,10 @@ def prepare(dataset: AbstractDataset):
     grid_features = torch.cat((grid_xy, geopotential, grid_border_mask), dim=1) # (N_grid, 4)
 
     torch.save(grid_features, graph_dir_path / "grid_features.pt")
+
+
+if __name__ == "__main__":
+    # Load Dataset
+    hparams = TitanParams()
+    dataset = TitanDataset(hparams)
+    prepare(dataset)
