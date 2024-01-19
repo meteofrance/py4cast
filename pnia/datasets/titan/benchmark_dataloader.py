fa3ecd1cac45bb3d5902f0fc01b9924d9eebd3b6
Frank Guibert <frank.guibert@meteo.fr>
2024-01-19 08:44:17 +0000

    update benchmark

--- pnia/datasets/titan/benchmark_dataloader.py
+++ pnia/datasets/titan/benchmark_dataloader.py
@@ -2,6 +2,7 @@
 Utility script to measure the performances
 of our dataloader when using multiple CPU cores
 """
+from itertools import cycle
 from torch.utils.data import DataLoader
 from mfai.torch.transforms import ToTensor
 from torchvision import transforms
@@ -14,31 +15,34 @@ if __name__ == "__main__":
 
     parser = ArgumentParser(TitanHyperParams)
     hparams = parser.parse_args()
+    batch_size=4
 
     print("hparams : ", hparams)
     dataset = TitanDataset(hparams)
-    transform = transforms.Compose(
-            [
-                ToTensor(),
-            ]
-        )
+
     loader = DataLoader(
             dataset,
-            batch_size=4,
+            batch_size=8,
             num_workers=hparams.num_workers,
-            shuffle=False,
+            shuffle=True,
             # https://discuss.pytorch.org/t/error-expected-more-than-1-value-per-channel-when-training/26274/2
             drop_last=True,
+            pin_memory=True,
         )
 
-    count = 0
     start_time = time.time()
-    nb_samples = 1000
-    for _ in loader:
-        count += 1
-        if count >= nb_samples:
-            break
+    no_iters = 100
+    data_iter = iter(loader) 
+    for i in range(no_iters):
+        try:
+            batch = next(data_iter)
+        except StopIteration:
+            data_iter = iter(loader)
+            batch = next(data_iter)
+        print(i)
+
+    print(i)
     stop_time = time.time()
     delta = stop_time - start_time
-    speed = nb_samples/delta
+    speed = no_iters*batch_size/delta
     print(f"Loading speed: {speed} sample(s)/sec")
