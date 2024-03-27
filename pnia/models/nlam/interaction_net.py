68287e790be74b8a48669ab5b55c9c1c20360c6a
Vincent Chabot <vincent.chabot@meteo.fr>
2024-03-27 09:29:20 +0100

    Cleaning

--- pnia/models/nlam/interaction_net.py
+++ pnia/models/nlam/interaction_net.py
@@ -2,7 +2,7 @@ import torch
 import torch_geometric as pyg
 from torch import nn
 
-from pnia.models.utils import CheckpointWrapper
+from pnia.models.base import CheckpointWrapper
 
 
 class InteractionNet(pyg.nn.MessagePassing):
