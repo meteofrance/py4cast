1af3908bb13eaf73a0b2752d668b6a1a53ad8bbd
Vincent Chabot <vincent.chabot@meteo.fr>
2024-04-08 13:19:15 +0200

    enable reloading a model for next train.

--- bin/train.py
+++ bin/train.py
@@ -113,8 +113,10 @@ def main(
         limit_train_batches=tp.limit_train_batches,
         limit_val_batches=tp.limit_train_batches,  # No reason to spend hours on validation if we limit the training.
     )
-
-    lightning_module = AutoRegressiveLightning(hp)
+    if args.load:
+        lightning_module = AutoRegressiveLightning.load_from_checkpoint(args.load,hparams=hp)
+    else:
+        lightning_module = AutoRegressiveLightning(hp)
 
     # Train model
     trainer.fit(
@@ -228,6 +230,10 @@ if __name__ == "__main__":
         default=False,
         help="When activated, log are not stored and models are not saved. Use in dev mode.",
     )
+    parser.add_argument(
+        "--load", 
+        type=str, 
+        help="Path to load model parameters from (default: None)")
     args, other = parser.parse_known_args()
 
     # Raise an exception if there are unknown arguments
