e616c24cffebd71bb9dfba5abee3b12c08e41c88
Vincent Chabot <vincent.chabot@meteo.fr>
2024-04-09 08:51:30 +0200

    reformat

--- bin/infer.py
+++ bin/infer.py
@@ -16,9 +16,7 @@ if __name__ == "__main__":
     # ckpt = "/home/mrpa/chabotv/pnia/saved_models/smeagol_franmgsp32graph-031914:46-8984/last.ckpt"
     # )
 
-    train_ds, val_ds, test_ds = get_datasets(
-        "smeagol", 2, 1, 17
-    )
+    train_ds, val_ds, test_ds = get_datasets("smeagol", 2, 1, 17)
 
     ckpt = "/home/mrpa/chabotv/pnia/saved_models/smeagol_franmgsp32hilam-040520:02-4832/last.ckpt"
 
