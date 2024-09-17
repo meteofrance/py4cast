1bb4ef977d2040b08ef60b41e4b5c3df968b1a55
VINCENT, LIONEL <lionel.vincent.dm@atos.net>
2024-09-17 14:21:04 +0200

    add a setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# Build the long description from the README.md file
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+# Build the requirements list from the requirements.txt file
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
+
+# Instal the sources as a lib
+setup(
+    name="py4cast",
+    version="0.1.0",
+    author="PN-IA Météo France",
+    description="Library to train a variety of Neural Network architectures "
+                "on various weather forecasting datasets.",
+    long_description=long_description,
+    packages=find_packages(),
+    python_requires='>=3.10',
+    install_requires=required
+)
