7af9f70519786eae11eaa3d0e8f9fb801f423af0
Lea Berthomier <lea.berthomier@meteo.fr>
2024-09-19 09:08:02 +0000

    lint + add gifs

--- bin/gif_comparison.py
+++ bin/gif_comparison.py
@@ -10,7 +10,7 @@ options:
   --date DATE           Date for inference. Format YYYYMMDDHH.
   --num_pred_steps NUM_PRED_STEPS
                         Number of auto-regressive steps/prediction steps.
-                        
+
 example: python bin/gif_comparison.py --ckpt AROME --ckpt /.../logs/my_run/epoch=247.ckpt --date 2023061812 --num_pred_steps 10"""
 
 import argparse
@@ -39,12 +39,16 @@ COLORMAPS = {
     "t2m": {"cmap": "Spectral_r", "vmin": 240, "vmax": 320},
     "r2": {"cmap": "Spectral", "vmin": 0, "vmax": 100},
     "tp": {"cmap": "Spectral_r", "vmin": 0.5, "vmax": 100},
-    "u10": {"cmap": "RdBu","vmin": -20, "vmax": 20},
-    "v10": {"cmap": "RdBu", "vmin": -20, "vmax": 20}
+    "u10": {"cmap": "RdBu", "vmin": -20, "vmax": 20},
+    "v10": {"cmap": "RdBu", "vmin": -20, "vmax": 20},
 }
 
 
-def downscale(array:np.ndarray, grid: str = "PAAROME_1S40", domain: Tuple[int]=[100, 612, 240, 880]) -> np.ndarray:
+def downscale(
+    array: np.ndarray,
+    grid: str = "PAAROME_1S40",
+    domain: Tuple[int] = [100, 612, 240, 880],
+) -> np.ndarray:
     """Downscales an array from Titan grid 1S100 to another grid and subdomain."""
     grid_info = METADATA["GRIDS"][grid]
     array = resize(array, grid_info["size"], anti_aliasing=True)
@@ -52,23 +56,23 @@ def downscale(array:np.ndarray, grid: str = "PAAROME_1S40", domain: Tuple[int]=[
     return array
 
 
-def get_param(path:Path, param:str) -> np.ndarray:
+def get_param(path: Path, param: str) -> np.ndarray:
     """Extracts a weather param from an AROME forecast in grib."""
     ds = xr.open_dataset(path, engine="cfgrib")
     array = ds[param].values
     arr_list = [downscale(array[t]) for t in range(array.shape[0])]
-    array = np.stack(arr_list)[:,::-1]
+    array = np.stack(arr_list)[:, ::-1]
     return array
 
 
-def post_process_tp_arome(array:np.ndarray) -> np.ndarray:
+def post_process_tp_arome(array: np.ndarray) -> np.ndarray:
     """Converts AROME precip forecast in mm/h.
     By default, AROME accumulates mm starting from t0."""
-    diff_arrs = [array[t+1] - array[t] for t in range(12)]
+    diff_arrs = [array[t + 1] - array[t] for t in range(12)]
     return np.stack(diff_arrs)
 
 
-def read_arome(date:str) -> Tuple[np.ndarray]:
+def read_arome(date: str) -> Tuple[np.ndarray]:
     """Extracts 5 parameters (t2m, r2, tp, u10, v10) of an AROME forecast."""
     path = AROME_PATH / date
     r2 = get_param(path / "AROME_1S100_ECH0_2M.grib", "r2")
@@ -80,7 +84,9 @@ def read_arome(date:str) -> Tuple[np.ndarray]:
     return t2m, r2, tp, u10, v10
 
 
-def get_model_and_hparams(ckpt: Path, num_pred_steps:int) -> Tuple[AutoRegressiveLightning, ArLightningHyperParam]:
+def get_model_and_hparams(
+    ckpt: Path, num_pred_steps: int
+) -> Tuple[AutoRegressiveLightning, ArLightningHyperParam]:
     """Loads a model from its checkpoint and changes the nb of forecast steps."""
     model = AutoRegressiveLightning.load_from_checkpoint(ckpt)
     hparams = model.hparams["hparams"]
@@ -89,19 +95,19 @@ def get_model_and_hparams(ckpt: Path, num_pred_steps:int) -> Tuple[AutoRegressiv
     return model, hparams
 
 
-def get_item_for_date(date:str, hparams: ArLightningHyperParam) -> Item:
+def get_item_for_date(date: str, hparams: ArLightningHyperParam) -> Item:
     """Returns an Item containing one sample for a chosen date.
     Date should be in format YYYYMMDDHH.
     """
     config_override = {"periods": {"test": {"start": date, "end": date}}}
     _, _, test_ds = get_datasets(
-            hparams.dataset_name,
-            hparams.num_input_steps,
-            hparams.num_pred_steps_train,
-            hparams.num_pred_steps_val_test,
-            hparams.dataset_conf,
-            config_override=config_override,
-        )
+        hparams.dataset_name,
+        hparams.num_input_steps,
+        hparams.num_pred_steps_train,
+        hparams.num_pred_steps_val_test,
+        hparams.dataset_conf,
+        config_override=config_override,
+    )
     item = test_ds[0]
     return item
 
@@ -119,21 +125,32 @@ def make_forecast(model: AutoRegressiveLightning, item: Item) -> torch.tensor:
     return forecast[0]
 
 
-def post_process_outputs(y: torch.tensor, feature_names:List[str], feature_names_to_idx:dict) -> np.ndarray:
+def post_process_outputs(
+    y: torch.tensor, feature_names: List[str], feature_names_to_idx: dict
+) -> np.ndarray:
     """Post-processes one forecast by de-normalizing the values of each feature."""
     arrays = []
     for feature_name in feature_names:
         idx_feature = feature_names_to_idx[feature_name]
         mean = hparams.dataset_info.stats[feature_name]["mean"]
         std = hparams.dataset_info.stats[feature_name]["std"]
-        y_norm = (y[:,:,:, idx_feature] * std + mean).cpu().detach().numpy()
+        y_norm = (y[:, :, :, idx_feature] * std + mean).cpu().detach().numpy()
         arrays.append(y_norm)
     return np.stack(arrays, axis=-1)
 
 
 @gif.frame
-def plot_frame(feature_name: str, target: np.ndarray, predictions: List[np.ndarray], domain_info: DomainInfo,
-    title:str=None, models_names:List[str]=None, colorbar_label:str=None, vmin:float=None, vmax:float=None)-> None:
+def plot_frame(
+    feature_name: str,
+    target: np.ndarray,
+    predictions: List[np.ndarray],
+    domain_info: DomainInfo,
+    title: str = None,
+    models_names: List[str] = None,
+    colorbar_label: str = None,
+    vmin: float = None,
+    vmax: float = None,
+) -> None:
     """Plots one frame of the animation."""
 
     nb_preds = len(predictions) + 1
@@ -152,20 +169,27 @@ def plot_frame(feature_name: str, target: np.ndarray, predictions: List[np.ndarr
 
     fig = plt.figure(constrained_layout=True, figsize=(4 * cols, 5 * lines), dpi=200)
     subfig = fig.subfigures(nrows=1, ncols=1)
-    axes = subfig.subplots(nrows=lines, ncols=cols, subplot_kw={"projection": domain_info.projection})
+    axes = subfig.subplots(
+        nrows=lines, ncols=cols, subplot_kw={"projection": domain_info.projection}
+    )
     extent = domain_info.grid_limits
 
     for i, data in enumerate([target] + predictions):
         axes[i].coastlines()
-        if param == "tp": # precipitations
+        if param == "tp":  # precipitations
             data = np.where(data < 0.5, np.nan, data)
         im = axes[i].imshow(
-            data, origin="lower", extent=extent, vmin=vmin, vmax=vmax, cmap=cmap,
+            data,
+            origin="lower",
+            extent=extent,
+            vmin=vmin,
+            vmax=vmax,
+            cmap=cmap,
         )
         if models_names:
             axes[i].set_title(models_names[i], size=15)
 
-    subfig.colorbar(im, ax=axes, location='bottom', label=colorbar_label, aspect=40)
+    subfig.colorbar(im, ax=axes, location="bottom", label=colorbar_label, aspect=40)
 
     if title:
         fig.suptitle(title, size=20)
@@ -174,7 +198,14 @@ def plot_frame(feature_name: str, target: np.ndarray, predictions: List[np.ndarr
     fig.text(0, 0.02, copyright, fontsize=8, ha="left")
 
 
-def make_gif(feature: str, date:str, target: np.ndarray, preds: List[np.ndarray], models_names: List[str], domain_info: DomainInfo):
+def make_gif(
+    feature: str,
+    date: str,
+    target: np.ndarray,
+    preds: List[np.ndarray],
+    models_names: List[str],
+    domain_info: DomainInfo,
+):
     """Plots a gifs comparing multiple forecasts of one feature."""
     vmin, vmax = target.min(), target.max()
     date = dt.datetime.strptime(date, "%Y%m%d%H")
@@ -187,16 +218,33 @@ def make_gif(feature: str, date:str, target: np.ndarray, preds: List[np.ndarray]
     for t in trange(target.shape[0]):
         title = f"{date_str} +{t+1}h"
         preds_t = [pred[t] for pred in preds]
-        frame = plot_frame(feature, target[t], preds_t, domain_info, title, models_names, unit, vmin, vmax)
+        frame = plot_frame(
+            feature,
+            target[t],
+            preds_t,
+            domain_info,
+            title,
+            models_names,
+            unit,
+            vmin,
+            vmax,
+        )
         frames.append(frame)
     gif.save(frames, f"{args.date}_{feature}.gif", duration=500)
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser("Plot animations")
-    parser.add_argument("--ckpt", type=str, action='append', help="Paths to the model checkpoint or AROME", required=True)
-    parser.add_argument("--date", type=str, help="Date for inference. Format YYYYMMDDHH.",  required=True)
+    parser.add_argument(
+        "--ckpt",
+        type=str,
+        action="append",
+        help="Paths to the model checkpoint or AROME",
+        required=True,
+    )
+    parser.add_argument(
+        "--date", type=str, help="Date for inference. Format YYYYMMDDHH.", required=True
+    )
     parser.add_argument(
         "--num_pred_steps",
         type=int,
@@ -205,7 +253,13 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    feature_names = ['aro_t2m_2m', 'aro_r2_2m', 'aro_tp_0m', 'aro_u10_10m', 'aro_v10_10m']
+    feature_names = [
+        "aro_t2m_2m",
+        "aro_r2_2m",
+        "aro_tp_0m",
+        "aro_u10_10m",
+        "aro_v10_10m",
+    ]
 
     y_preds = []
     models_names = []
@@ -235,6 +289,13 @@ if __name__ == "__main__":
     for feature_name in feature_names:
         print(feature_name)
         idx_feature = feature_idx_dict[feature_name]
-        target_feat = y_true[:,:,:, idx_feature]
-        list_preds_feat = [pred[:,:,:,idx_feature] for pred in y_preds]
-        make_gif(feature_name, args.date, target_feat, list_preds_feat, models_names, domain_info)
+        target_feat = y_true[:, :, :, idx_feature]
+        list_preds_feat = [pred[:, :, :, idx_feature] for pred in y_preds]
+        make_gif(
+            feature_name,
+            args.date,
+            target_feat,
+            list_preds_feat,
+            models_names,
+            domain_info,
+        )
