6379211e9660e3ca96e255f31b936ca52653656d
colon3ltocard <colon3l.tocard@gmail.com>
2024-09-23 19:30:09 +0000

    re-enable Accuracy Metric

--- py4cast/lightning.py
+++ py4cast/lightning.py
@@ -213,7 +213,7 @@ class AutoRegressiveLightning(pl.LightningModule):
         max_pred_step = self.hparams["hparams"].num_pred_steps_val_test - 1
         self.rmse_psd_plot_metric = MetricPSDVar(pred_step=max_pred_step)
         self.psd_plot_metric = MetricPSDK(save_path, pred_step=max_pred_step)
-        # self.acc_metric = MetricACC(self.hparams["hparams"].dataset_info)
+        self.acc_metric = MetricACC(self.hparams["hparams"].dataset_info)
 
     @rank_zero_only
     def log_hparams_tb(self):
@@ -509,7 +509,7 @@ class AutoRegressiveLightning(pl.LightningModule):
                 plotter.update(self, prediction=prediction, target=target)
             self.psd_plot_metric.update(prediction, target, self.original_shape)
             self.rmse_psd_plot_metric.update(prediction, target, self.original_shape)
-            # self.acc_metric.update(prediction, target)
+            self.acc_metric.update(prediction, target)
 
     def on_validation_epoch_end(self):
         """
@@ -520,7 +520,7 @@ class AutoRegressiveLightning(pl.LightningModule):
         dict_metrics = dict()
         dict_metrics.update(self.psd_plot_metric.compute())
         dict_metrics.update(self.rmse_psd_plot_metric.compute())
-        # dict_metrics.update(self.acc_metric.compute())
+        dict_metrics.update(self.acc_metric.compute())
         for name, elmnt in dict_metrics.items():
             if isinstance(elmnt, matplotlib.figure.Figure):
                 self.logger.experiment.add_figure(f"{name}", elmnt, self.current_epoch)
@@ -576,10 +576,9 @@ class AutoRegressiveLightning(pl.LightningModule):
         # Notify plotters & metrics
         for plotter in self.test_plotters:
             plotter.update(self, prediction=prediction, target=target)
-        # self.acc_metric.update(prediction, target)
+        self.acc_metric.update(prediction, target)
         self.psd_plot_metric.update(prediction, target, self.original_shape)
         self.rmse_psd_plot_metric.update(prediction, target, self.original_shape)
-        # self.acc_metric.update(prediction, target)
 
     @cached_property
     def interior_2d(self) -> torch.Tensor:
@@ -603,7 +602,7 @@ class AutoRegressiveLightning(pl.LightningModule):
         # and: https://github.com/Lightning-AI/pytorch-lightning/issues/18803
         self.psd_plot_metric.compute()
         self.rmse_psd_plot_metric.compute()
-        # self.acc_metric.compute()
+        self.acc_metric.compute()
         # Notify plotters that the test epoch end
         for plotter in self.test_plotters:
             plotter.on_step_end(self, label="Test")
