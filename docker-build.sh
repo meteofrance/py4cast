363d9c02de16e7ac76b23dc91e2b1586b237cb6f
VINCENT, LIONEL <lionel.vincent.dm@atos.net>
2024-10-17 09:18:24 +0200

    add podman in build script and doc

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,33 @@
+# Parse script options
+FORMAT="docker"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -f|--format)
+      FORMAT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+if [[ ${FORMAT} = "podman" ]]; then
+    BUILDER=$(which podman)
+    OPTIONS="--storage-opt overlay.mount_program=/usr/bin/fuse-overlayfs"
+elif [[ ${FORMAT} = "docker" ]]; then
+    BUILDER=$(which docker)
+else
+    echo "Format \"${FORMAT}\" not supported, please choose in {docker, podman}."
+    exit 2
+fi
+
 # Automatically retrieve the CUDA and torch version from env var, otherwise set them from reference file env.yaml
 if [[ -z "${PY4CAST_CUDA_VERSION}" ]]; then
   PY4CAST_CUDA_VERSION=$(cat env.yaml | grep pytorch-cuda= | sed 's/.*pytorch-cuda=\([0-9]*\.[0-9]\).*/\1/')
@@ -15,7 +45,8 @@ fi
 TAG=$(git rev-parse --short HEAD)
 
 # Build the py4cast docker image
-docker build \
+${BUILDER} build \
+    ${OPTIONS} \
     --build-arg CUDA_VERS=${PY4CAST_CUDA_VERSION} \
     --build-arg TORCH_VERS=${PY4CAST_TORCH_VERSION} \
     --build-arg USERNAME=$(id -un) \
