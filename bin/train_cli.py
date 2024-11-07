2149fdcaf9ee0c10a633f956157a60ed86eadbff
Corentin <koseznec@gmail.com>
2024-11-07 08:22:55 +0000

    useful commment

--- bin/train_cli.py
+++ bin/train_cli.py
@@ -6,21 +6,24 @@ from py4cast.lightning import (
 )
 
 
+# install : pip install -U 'jsonargparse[signatures]>=4.27.7'
+# Launch : python bin/train_cli.py fit --config config/CLI/test.yaml 
+
 class MyCLI(LightningCLI):
     def __init__(self, model_class, datamodule_class):
         super().__init__(model_class, datamodule_class)
 
-    def add_arguments_to_parser(self, parser):
-        parser.link_arguments(
-            "data.train_dataset_info.weather_dim",
-            "model.hparams.dataset_info.weather_dim",
-            apply_on="instantiate",
-        )
-        parser.link_arguments(
-            "data.train_dataset_info.forcing_dim",
-            "model.hparams.dataset_info.forcing_dim",
-            apply_on="instantiate",
-        )
+    # def add_arguments_to_parser(self, parser):
+    #     parser.link_arguments(
+    #         "data.train_dataset_info.weather_dim",
+    #         "model.hparams.dataset_info.weather_dim",
+    #         apply_on="instantiate",
+    #     )
+    #     parser.link_arguments(
+    #         "data.train_dataset_info.forcing_dim",
+    #         "model.hparams.dataset_info.forcing_dim",
+    #         apply_on="instantiate",
+    #     )
         # parser.link_arguments("data.len_train_dl", "model.hparams.len_train_loader", apply_on="instantiate")
         pass
 
