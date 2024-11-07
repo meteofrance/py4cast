5d3452920a5fd61252f34a3444a515e07e0f613f
clemoule <161027136+clemoule@users.noreply.github.com>
2024-11-07 14:26:55 +0100

    Update conv.py

--- py4cast/models/vision/conv.py
+++ py4cast/models/vision/conv.py
@@ -19,6 +19,7 @@ from py4cast.models.vision.utils import features_last_to_second, features_second
 @dataclass_json
 @dataclass(slots=True)
 class HalfUnetSettings:
+
     num_filters: int = 64
     dilation: int = 1
     bias: bool = False
@@ -210,6 +211,7 @@ class _HalfUnet(ModelABC, nn.Module):
                 )
             )
         else:
+
             return nn.Sequential(
                 OrderedDict(
                     [
@@ -257,6 +259,7 @@ class HalfUnet(_HalfUnet):
 @dataclass_json
 @dataclass(slots=True)
 class UnetSettings:
+
     init_features: int = 64
 
 
