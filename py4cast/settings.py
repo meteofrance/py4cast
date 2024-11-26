923014eee5706cf33d256fcc9606a172f2b0f0ce
leborgnelarivierec <clement.leborgne-lariviere@meteo.fr>
2024-11-26 14:38:30 +0100

    delete hiera_dir path in settings

--- py4cast/settings.py
+++ py4cast/settings.py
@@ -3,4 +3,3 @@ from pathlib import Path
 
 ROOTDIR = Path(os.environ.get("PY4CAST_ROOTDIR", "/scratch/shared/py4cast"))
 CACHE_DIR = ROOTDIR / "cache"
-HIERA_MASKED_DIR = ROOTDIR / "models" / " Hiera_masked"
