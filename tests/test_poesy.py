36a6411a8edfa23973aa14b95dc1022a51680a9c
Corentin <corentin.seznec@meteo.fr>
2024-11-28 14:10:47 +0000

    comment test_poesy

--- tests/test_poesy.py
+++ tests/test_poesy.py
@@ -5,7 +5,7 @@ def test_run_through_timestamps():
     """
     Test the browse_dataset function
     """
-    ### Exemple 1
+    # Exemple 1
     period = Period(start="2021061500", end="2021061503", step=2, name="test")
 
     term = {"start": 0, "end": 11, "timestep": 2}
