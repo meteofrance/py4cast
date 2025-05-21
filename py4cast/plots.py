902614ded7c3d9384ab1a7ba3ef9a98e1c84e2c9
FERREIRA mathilde <mathilde.ferreira@meteo.fr>
2025-05-21 13:49:31 +0200

    fix bug in plot

--- py4cast/plots.py
+++ py4cast/plots.py
@@ -298,8 +298,8 @@ class MapPlot(Plotter):
             )
             prediction_rescaled = pred * std + mean
             target_rescaled = targ * std + mean
-            batch_copy.input = batch_copy.inputs * std + mean
-            batch_copy.forcing = batch_copy.forcing * std + mean
+            batch_copy.input.tensor = batch_copy.inputs.tensor * std + mean
+            batch_copy.forcing.tensor = batch_copy.forcing.tensor * std + mean
 
             # Iterate over the examples
             # We assume examples are already on grid
